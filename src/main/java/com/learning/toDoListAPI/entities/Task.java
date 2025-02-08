e6375c94ec4997a0ccd6221817653ea00274b098
Lucas David <santoslucasdavid@gmail.com>
2025-02-08 16:56:54 -0300

    JPA mapping on Task entity

--- src/main/java/com/learning/toDoListAPI/entities/Task.java
+++ src/main/java/com/learning/toDoListAPI/entities/Task.java
@@ -1,15 +1,19 @@
 package com.learning.toDoListAPI.entities;
 
 import com.learning.toDoListAPI.enums.TaskStatus;
+import jakarta.persistence.*;
 
-import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_task")
 public class Task implements Serializable {
 
-    @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private TaskStatus status;
