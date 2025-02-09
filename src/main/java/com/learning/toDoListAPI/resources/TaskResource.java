728cb10b8548686ecdd2af955f41b7b15745d9ed
Lucas David <santoslucasdavid@gmail.com>
2025-02-09 20:05:59 -0300

    Service layer and component registration added

--- src/main/java/com/learning/toDoListAPI/resources/TaskResource.java
+++ src/main/java/com/learning/toDoListAPI/resources/TaskResource.java
@@ -2,18 +2,32 @@ package com.learning.toDoListAPI.resources;
 
 import com.learning.toDoListAPI.entities.Task;
 import com.learning.toDoListAPI.enums.TaskStatus;
+import com.learning.toDoListAPI.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/tasks")
 public class TaskResource {
 
+    @Autowired
+    private TaskService service;
+
     @GetMapping
-    public ResponseEntity<Task> findAll() {
-        Task t = new Task(1L, "Fazer compras", TaskStatus.PENDENTE);
-        return ResponseEntity.ok().body(t);
+    public ResponseEntity<List<Task>> findAll() {
+        List<Task> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Task> findById(@PathVariable Long id){
+        Task obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/learning/toDoListAPI/services/TaskService.java
@@ -0,0 +1,26 @@
+package com.learning.toDoListAPI.services;
+
+import com.learning.toDoListAPI.entities.Task;
+import com.learning.toDoListAPI.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository repository;
+
+    public List<Task> findAll() {
+        return repository.findAll();
+    }
+
+    public Task findById(Long id) {
+        Optional<Task> obj = repository.findById(id);
+        return obj.get();
+    }
+}
